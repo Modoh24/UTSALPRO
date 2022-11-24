9d0928aadfda658f1c577beadd1b4f1b67bfdb32
Modoh24 <115127427+Modoh24@users.noreply.github.com>
2022-11-24 10:56:32 +0700

    Add files via upload

---
+++ Mohdo.java
@@ -0,0 +1,18 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+            String nama = "Lorina Mohdo";
+            int umur = 2003;
+
+            System.out.println("Assalamualaikum");
+            System.out.println("nama saya :"+ nama);
+            System.out.println("saya lahir tahun :"+ umur);
+      }
+}
